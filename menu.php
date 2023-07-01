dc6b16c4d8152e033a5132720da866d13f3539db
Laishyde <laiscristina.animamundi@gmail.com>
2023-07-01 16:27:04 -0300

    iniciando projeto

---
+++ menu.php
@@ -0,0 +1,11 @@
+<header>
+    <ul>
+        <li>contribua aqui</li>
+        <li><input type="text" placeholder="Pesquisar por projetos"></li>
+        <li>início</li>
+        <li>projetos</li>
+        <li>como funciona</li>
+        <li>contato</li>
+        <li><button>novo projeto</button></li>
+    </ul>
+</header>
